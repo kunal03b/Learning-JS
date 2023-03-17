3c0767fde08f5d3f73de6a0c2c9f18c648fc4c45
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-18 02:19:41 +0530

    Variable initial commit

---
+++ variables.js
@@ -0,0 +1,22 @@
+var firstName = "Kunal";
+var age = 19;
+var isMale = true;
+var balance = 11.12
+var dob = new Date(2003,11,11);
+var person = {}
+var empty = undefined
+
+console.log(firstName);
+console.log(typeof firstName);
+console.log(age);
+console.log(typeof age);
+console.log(isMale);
+console.log(typeof isMale);
+console.log(balance);
+console.log(typeof balance);
+console.log(dob);
+console.log(typeof dob);
+console.log(person);
+console.log(typeof person);
+console.log(empty);
+console.log(typeof empty);
