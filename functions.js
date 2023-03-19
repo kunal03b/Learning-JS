43cbf9e3d547b5fff74bc1a56549a83a1d31313b
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-19 22:12:30 +0530

    error solved

--- functions.js
+++ functions.js
@@ -1,14 +1,10 @@
 // functions
 
 function func(num1, num2){
-    // var addition = 2+3;
-    // var subtract = 3-2;
-    var addition = num1 + num2;
-    var subtract = num1 - num2;
-    return addition ,subtract
-
+    addition = num1 + num2;
+    return addition;
 }
 var result1  = func(20,10)
-var result2 = func(70,60)
+var result2 = func(70,10)
 
 console.log(result1, result2)
