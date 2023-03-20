fd32f10967c194bab1de0f7b6543abd55ad0bf8e
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-20 22:53:47 +0530

    problem2 solved

--- functionsProblems.js
+++ functionsProblems.js
@@ -1,7 +1,7 @@
 // write a javascript program to check two numbers and return true if one of the number is 100 or if the sum of the two number is 100.
 
-var firstNo = 50
-var secondNo = 50
+var firstNo = 20
+var secondNo = 100
 
 const myFunc = function(a, b) {
     if(a ==100 || b == 100 || a + b ==100 ){
@@ -11,5 +11,10 @@ const myFunc = function(a, b) {
         return false;
     }
 }
-
 console.log(myFunc(firstNo, secondNo))
+
+// ----------------------------------------------------------------------------------
+// WAP to get the extension of a filename.
+const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
+console.log(getFileExtension('index.html'));
+console.log(getFileExtension('webpack.config.js'));
