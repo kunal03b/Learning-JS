244f00879d754d09892c7b87f3e5be530ce2e422
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-25 14:45:33 +0530

    increment button worked

--- Passenger Counter/index.js
+++ Passenger Counter/index.js
@@ -1,6 +1,10 @@
 // document.getElementById("count-el").innerText = 5
 
-count = 0;
+let countEl = document.getElementById("count-el")
+
+let count = 0;
 function increment(){
-    
+    count = count+1;
+    countEl.innerText = count;
+    console.log(count);
 }
