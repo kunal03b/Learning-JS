7b1ea9956d0d711c81e11b32f583bc9a9feb492e
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-25 15:20:01 +0530

    completed

--- Passenger Counter/index.js
+++ Passenger Counter/index.js
@@ -1,10 +1,17 @@
-// document.getElementById("count-el").innerText = 5
-
+let count = 0
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 
-let count = 0;
-function increment(){
-    count = count+1;
-    countEl.innerText = count;
-    console.log(count);
+function increment() {
+    count += 1
+    countEl.textContent = count
 }
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContenttStr
+    countEl.textContent = 0
+    count = 0
+}
+
+console.log("Let's count people on the subway!")
