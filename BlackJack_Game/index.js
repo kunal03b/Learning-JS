d801e083ebb363ed1211c31b4ae4c11b86aef0d7
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-26 23:02:39 +0530

    message shown on screen

--- BlackJack_Game/index.js
+++ BlackJack_Game/index.js
@@ -1,25 +1,27 @@
-var firstCard = 2
-var secondCard = 19
+var firstCard = 11
+var secondCard = prompt("Enter the 2nd card number");
+let sum = firstCard + secondCard;
 let hasBlackJack = false
 let isAlive = true
+let message = " "
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
 
-let sum = firstCard + secondCard;
-
-if(sum < 21){
-    console.log("Do you want to draw a new card?");
-    // hasBlackJack = false;
-    // isAlive = true;
+function startGame(){
+    if(sum < 21){
+        message = "Do you want to draw a new card?"
+        // hasBlackJack = false;
+        // isAlive = true;
+    }
+    else if(sum === 21){
+        message = "Congratulations! You've got BlackJack!"
+        hasBlackJack = true;
+        // isAlive = true;
+    }
+    else{
+        message = "You are out of the game!"
+        // hasBlackJack = false;
+        isAlive = false;
+    }
+    messageEl.textContent = message
 }
-else if(sum === 21){
-    console.log("Congratulations! You've got BlackJack!");
-    hasBlackJack = true;
-    // isAlive = true;
-}
-else{
-    console.log("You are out of the game!");
-    // hasBlackJack = false;
-    isAlive = false;
-}
-
-console.log(isAlive)
-console.log(hasBlackJack)
