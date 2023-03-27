9903777484636df90c2bef8fd79fefcf78ce00d5
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-27 14:27:25 +0530

    cards updated

--- BlackJack_Game/index.js
+++ BlackJack_Game/index.js
@@ -8,8 +8,10 @@ let message = " "
 let messageEl = document.getElementById("message-el")
 // let sumEl = document.getElementById("sum-el")
 let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard + " "
     sumEl.textContent = "Sum: " + sum
     if(sum < 21){
         message = "Do you want to draw a new card?"
