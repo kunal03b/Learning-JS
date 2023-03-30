d8d7932676826487b54e322033be010763990d41
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-30 11:48:34 +0530

    array initial commit

---
+++ arrays.js
@@ -0,0 +1,21 @@
+// Primitive vs reference data types
+// Privitive types
+let str1 = "Kunal"
+let str2 = str1
+console.log(str1)
+console.log(str2)
+
+str1 = "Sharma"
+console.log(str1)
+console.log(str2)
+
+// Reference Data types
+
+let arr1 = ["item1","item2"]
+let arr2 = arr1
+console.log(arr1)
+console.log(arr2)
+
+arr1.push("item3")
+console.log(arr1)
+console.log(arr2)
