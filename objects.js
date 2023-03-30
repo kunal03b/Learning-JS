9ccdee303165553801c158a5003ecf14e6f011da
KUNAL SHARMA <kunal03b@gmail.com>
2023-03-30 18:18:00 +0530

    object initial

---
+++ objects.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "John",
+    rollNo: "1",
+    age: "22",
+    favFood: ["Chowmin", "Burger","Pizza"]
+}
+console.log(person)
+person.gender = "male";
+console.log(person)
