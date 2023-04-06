13b7fcdd11ab8dcf2a08498f091f6ae9ed69a10d
KUNAL SHARMA <kunal03b@gmail.com>
2023-04-06 21:55:22 +0530

    1 question done

---
+++ practice.js
@@ -0,0 +1,7 @@
+// program to multiply two values using arrow function
+function multiply(a, b) {
+    return a*b;
+}
+const multiplyValues = (a,b) => a*b;
+console.log(multiply(2,3))
+console.log(multiplyValues(2,3));
